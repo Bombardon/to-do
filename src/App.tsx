37c9076ab75c250503534a7419042b0f32cb37b6
Scott R Beaver <scottrbeaver@gmail.com>
2020-12-27 13:34:50 -0500

    Added input

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ const App = () => {
   return (
     <div className="App">
       <h1>To-do</h1>
+      <form>
+        Add task: &nbsp;
+        <input type="text" />
+      </form>
     </div>
   );
 }
