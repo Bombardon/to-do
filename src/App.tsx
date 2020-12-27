e7c2a15479ec1fe0d1f60bbd8ad7d87399146377
Scott Beaver <scottrbeaver@gmail.com>
2020-12-27 18:37:17 -0500

    Minor bad styling and split out task list/add task to their own components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,94 @@
+import React, { FC } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import './App.css';
 
 type Task = {
-  ID: string;
-  Description: string;
-  DueOn: Date;
+  ID: string,
+  Description: string,
 }
 
 type Tasks = Array<Task>;
 
-const tasks = new Tasks[
-  new Task({
-    ID: "1",
-    Description: "Task 1"
-  }),
-  new Task({
-    ID: "2",
-    Description: "Task 2"
-  }),
-];
+interface TaskListProps {
+  data: Tasks
+}
+
+interface AddTaskProps {
+  value: string,
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit?: () => void;   
+}
+
+const tasks: Task[] = [];
+//   {
+//     ID: "1",
+//     Description: 'Task 1',
+//   },
+//   {
+//     ID: "2",
+//     Description: 'Task 2',
+//   }
+// ];
 
 const App = () => {
+  const [list, setTaskList] = React.useState(tasks);
+  const [newTask, setNewTask] = React.useState('');
+
+  function handleChange(event : React.ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleAddTask() {
+    const updatedList = list.concat({ ID: uuidv4(), Description: newTask });
+    setTaskList(updatedList);
+    setNewTask('');
+  }
+
   return (
     <div>
-      <h1>To-do</h1>
-      <div>
-        <form>
-          Add task: &nbsp;
-          <input type="text" />
-        </form>
-      </div>
-      <div>
-        <h2>My tasks</h2>
-      </div>
-      
+      <h1 style={{fontSize:'50px'}}>To-do</h1>
+      <TaskList data={list} />
+      <AddTask 
+        value={newTask}
+        onChange={handleChange}
+        onSubmit={handleAddTask}
+      />
     </div>
   );
 }
 
-const TaskList = (list: Tasks) => {
-  {list.map(item => (
-    <TaskListItem {...item} />
-  ))};
-};
+const AddTask: FC<AddTaskProps> = ({value, onChange, onSubmit}) => (
+  <div>
+  Add task: &nbsp;
+  <input 
+    type="text" 
+    value={value}
+    onChange={onChange}
+  />
+  <button 
+    type='submit'
+    onClick={onSubmit}
+  >
+    Add
+  </button>
+</div>
+);
 
-const TaskListItem = (task: Task) => {
-  return (
-    <div>
-      <span>{task.Description}</span>
-    </div>
-  );
-};
+const TaskList: FC<TaskListProps> = ({ data }) => (
+  <div style={{boxShadow: '0 3px 7px 0 rgba(110, 112, 114, 0.21)'}}>
+    <h2>My tasks</h2>
+    {data.length > 0 &&
+      <ul>
+        {data.map((task) => (
+          <li key={task.ID}>{task.Description} <button>Complete</button></li>
+        ))}
+      </ul>
+    } 
+    {data.length === 0 &&
+      <span>You don't have any tasks yet.</span>
+    }
+  </div>
+);
 
 export default App;
