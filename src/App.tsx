e7bb73c1ab4933cd30c7118da2d4b3d8ae5b856f
Scott R Beaver <scottrbeaver@gmail.com>
2020-12-27 14:25:13 -0500

    Trying to fix tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 
+type Task = {
+  ID: string;
+  Description: string;
+  DueOn: Date;
+}
+
+type Tasks = Array<Task>;
+
+const tasks = new Tasks[
+  new Task({
+    ID: "1",
+    Description: "Task 1"
+  }),
+  new Task({
+    ID: "2",
+    Description: "Task 2"
+  }),
+];
+
 const App = () => {
   return (
-    <div className="App">
+    <div>
       <h1>To-do</h1>
-      <form>
-        Add task: &nbsp;
-        <input type="text" />
-      </form>
+      <div>
+        <form>
+          Add task: &nbsp;
+          <input type="text" />
+        </form>
+      </div>
+      <div>
+        <h2>My tasks</h2>
+      </div>
+      
     </div>
   );
 }
+
+const TaskList = (list: Tasks) => {
+  {list.map(item => (
+    <TaskListItem {...item} />
+  ))};
+};
+
+const TaskListItem = (task: Task) => {
+  return (
+    <div>
+      <span>{task.Description}</span>
+    </div>
+  );
+};
 
 export default App;
